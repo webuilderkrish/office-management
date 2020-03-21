ab9b2e9d7cb4ee41328bd7173fd111b803a57f15
shubhamdigole@gmail.com <shubhamdigole@gmail.com>
2020-03-21 17:17:37 +0530

    master finakized ui

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -2,11 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
  
 
+import { FormsModule } from '@angular/forms';
+import { MatCardModule, MatButtonModule, MatDialogModule, MatDialogActions, MatFormFieldModule, MatDividerModule, MatInputModule, MatToolbarModule, MatSidenavModule} from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponentComponent } from './login-component/login-component.component';
 import { UserComponentComponent } from './user-component/user-component.component';
 import { HomeComponentComponent } from './home-component/home-component.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,9 +19,18 @@ import { HomeComponentComponent } from './home-component/home-component.componen
     HomeComponentComponent
   ],
   imports: [
-  
-    BrowserModule,
+    MatDividerModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
     AppRoutingModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
